6edac572c5f545e43ce6bd1937519491ba335148
muhammadshams <mr.shamssheikh@gmail.com>
2020-01-27 01:34:17 +0500

    functionally complete

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -10,22 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var counterText: UILabel!
+    
     @IBOutlet weak var resetBtn: UIButton!
     @IBOutlet weak var play: UIButton!
     @IBOutlet weak var pause: UIButton!
     
+    var timer = Timer()
+    var count = 0.0
+    var isRunning=false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        counterText.text =  "\(count)"
+        play.isEnabled = true
+        pause.isEnabled = false
     }
 
     @IBAction func pause(_ sender: Any) {
+           play.isEnabled = true
+           pause.isEnabled = false
+        
+        timer.invalidate()
+        isRunning = false
+    
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+            timer.invalidate()
+            count = 0
+            play.isEnabled = true
+            pause.isEnabled = false
+            counterText.text =  "\(count)"
+            isRunning = false
+    
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(funUpdateTimer), userInfo: nil, repeats: true)
+        
+        play.isEnabled = false
+        pause.isEnabled = true
+        
+        isRunning = true
     }
     
     
-    @IBAction func play(_ sender: Any) {
+    @objc func funUpdateTimer(){
+        count += 0.1
+        counterText.text = String(format: "%.1f", count)
+        
     }
     
 }
